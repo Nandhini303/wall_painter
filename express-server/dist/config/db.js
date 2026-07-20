8cf4f7c8c9cb831cb4a4a0deded283d76fbd4f01
Nandhini303 <nandhini303303@gmail.com>
2026-07-20 13:24:06 +0530

    Fix: Remove secrets and node_modules from repository

--- express-server/dist/config/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectDB = void 0;
-const dbMonitor_1 = require("./dbMonitor");
-const connectDB = async () => {
-    const monitor = dbMonitor_1.MongoConnectionMonitor.getInstance();
-    await monitor.connectWithMonitor();
-};
-exports.connectDB = connectDB;
